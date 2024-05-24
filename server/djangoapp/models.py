2dd490a21fbb362d59e0dfae0ac36a4a6223226b
judithpp13 <judithyang13@gmail.com>
2024-05-24 13:45:57 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -36,14 +36,15 @@ class CarModel(models.Model):
     name = models.CharField(max_length=100)
     CAR_TYPES = [('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon')]
 
-       type = models.CharField(max_length=10, choices=CAR_TYPES,
+    type = models.CharField(max_length=10, choices=CAR_TYPES,
                             default='SUV')
     year = models.IntegerField(default=2023,
                                validators=[MaxValueValidator(2023),
                                MinValueValidator(2015)])
 
     def __str__(self):
-        return self.name  # Return the name as the string representation
+        return self.name  
+        # Return the name as the string representation
 
 
 # - Many-To-One relationship to Car Make model (One Car Make has many
