d2757751c8b263c565816a32fd6ed23ca19da32f
judithpp13 <judithyang13@gmail.com>
2024-06-01 09:49:38 -0400

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,6 +11,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 
+
 class CarMake(models.Model):
 
     name = models.CharField(max_length=100)
@@ -27,6 +28,7 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
 
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
@@ -34,16 +36,18 @@ class CarModel(models.Model):
     # Many-to-One relationship
 
     name = models.CharField(max_length=100)
-    CAR_TYPES = [('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon')]
+    CAR_TYPES = [("SEDAN", "Sedan"), ("SUV", "SUV"), ("WAGON", "Wagon")]
 
-    type = models.CharField(max_length=10, choices=CAR_TYPES,
-                            default='SUV')
-    year = models.IntegerField(default=2023,
-                               validators=[MaxValueValidator(2023), MinValueValidator(2015)])
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
 
     def __str__(self):
         return self.name
         # Return the name as the string representation
+
+
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
 # - Name
