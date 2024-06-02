b02306cd779c108b026fef3a14fe2e2e0d3d5b45
judithpp13 <judithyang13@gmail.com>
2024-06-01 22:18:26 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,8 +29,9 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','https://judithyang13-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = 
-['https://judithyang13-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS = [
+    "https://judithyang13-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/"
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
