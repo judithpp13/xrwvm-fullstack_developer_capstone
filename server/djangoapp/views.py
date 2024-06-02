618d82930386291c52efc1f98e0bb359b1eebff1
judithpp13 <judithyang13@gmail.com>
2024-06-02 15:34:33 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -160,10 +160,11 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as e:
             print(f"Error: {e}")
-            return JsonResponse({"status": 401, \
-                                 "message": "Error in posting review"})
+            return JsonResponse({"status": 401,
+            "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
